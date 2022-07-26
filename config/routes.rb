5862de81cbab20d5cf1e50f7368dd92df2a82a5a
JC De Belen <debelenjc@gmail.com>
2022-07-26 18:29:35 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
   # get 'home/index'
-  root 'home#index'
   resources :categories
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
